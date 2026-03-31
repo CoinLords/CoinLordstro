74d10980c17cf3118dcc4e30e36854c071fb3be4
CoinLords <eliot.rc@outlook.fr>
2026-03-31 10:41:01 +0200

    CoinLordstro v1.0.0
    Adds retextures for Mime, Acrobat and Oops! all 6 jokers

---
+++ CoinLordstro/main.lua
@@ -0,0 +1,58 @@
+--- CoinLordstro Texture Pack
+--- Requires Steamodded and Malverk.
+
+AltTexture({
+    key = 'mime',
+    set = 'Joker',
+    path = 'j_mime.png',
+    keys = { 'j_mime' },
+    display_pos = 'j_mime',
+    px = 71,
+    py = 95,
+    loc_txt = {
+        name = 'CoinLordstro – Mime',
+    },
+})
+
+AltTexture({
+    key = 'acrobat',
+    set = 'Joker',
+    path = 'j_acrobat.png',
+    keys = { 'j_acrobat' },
+    display_pos = 'j_acrobat',
+    px = 71,
+    py = 95,
+    loc_txt = {
+        name = 'CoinLordstro – Acrobat',
+    },
+})
+
+AltTexture({
+    key = 'oops',
+    set = 'Joker',
+    path = 'j_oops.png',
+    keys = { 'j_oops' },
+    display_pos = 'j_oops',
+    px = 71,
+    py = 95,
+    loc_txt = {
+        name = 'CoinLordstro – Oops! All 6s',
+    },
+})
+
+TexturePack({
+    key = 'CoinLordstro',
+    textures = {
+        'CoinLordstro_mime',
+        'CoinLordstro_acrobat',
+        'CoinLordstro_oops',
+    },
+    loc_txt = {
+        name = 'CoinLordstro',
+        text = {
+            'My custom art for',
+            'the coolest jokers',
+            'in balatro',
+        },
+    },
+})
